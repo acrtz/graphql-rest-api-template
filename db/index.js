5c151a4143089d20e706ddf922cde47731beff49
armandrtz <armandrtz@gmail.com>
2020-10-20 14:31:31 -0700

    fix parameters not being used correctly

--- db/index.js
+++ db/index.js
@@ -13,29 +13,34 @@ const articles = [
   },
 ];
 
-const fakeDBConnection = (request) =>
-  new Promise((resolve, reject) => setTimeout(() => resolve(request), 300));
+const fakeDBConnection = (request) => {
+  const response = request();
+  console.log({ response });
+  return new Promise((resolve, reject) =>
+    setTimeout(() => resolve(response), 300)
+  );
+};
 
-const getArticle = () =>
-  fakeDBConnection((id) => articles.find((article) => article.id === id));
+const getArticle = (id) =>
+  fakeDBConnection(() => articles.find((article) => article.id === id));
 
 const getArticles = () => fakeDBConnection(() => articles);
 
-const createArticle = () =>
-  fakeDBConnection((article) => {
+const createArticle = (article) =>
+  fakeDBConnection(() => {
     articles.push(article);
     return article.id;
   });
 
-const updateArticle = () =>
-  fakeDBConnection((article) => {
+const updateArticle = (article) =>
+  fakeDBConnection(() => {
     const index = articles.findIndex(({ index }) => index === article.index);
     articles[index] = article;
     return article.id;
   });
 
-const deleteArticle = () =>
-  fakeDBConnection((article) => {
+const deleteArticle = (article) =>
+  fakeDBConnection(() => {
     const index = articles.findIndex(({ index }) => index === article.index);
     articles.splice(index, 1) = article;
     return article.id;
